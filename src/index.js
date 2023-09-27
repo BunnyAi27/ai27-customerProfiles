9cee5ec02fb14c5f4fbb3517d5c68b623c4339d9
BunnyAi27 <135638399+BunnyAi27@users.noreply.github.com>
2023-09-27 23:24:20 +0200

    CDK stack paramaterised + code formated

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-const connectProfilesHelper = require('./helpers/connectProfilesHelper');
-const responseBuilder = require('./helpers/responseBuilder');
+const connectProfilesHelper = require("./helpers/connectProfilesHelper");
+const responseBuilder = require("./helpers/responseBuilder");
 
 exports.handler = async (event) => {
-    // TODO implement
-    console.log("Event", JSON.stringify(event))
+  // TODO implement
+  console.log("Event", JSON.stringify(event));
 
-    try {
-      switch (event.headers.channel.toLowerCase()) {
-        case 'connect':
-          return await connectProfilesHelper.connectProfileAction(event);
-        case 'thirdpart':
-          return await connectProfilesHelper.connectProfileAction(event);
-        default:
-          return await connectProfilesHelper.connectProfileAction(event)
-      }
-    } catch (error) {
-      console.error("index: error: ", error)
-      const errResp = {
-        errorMessage: "Check if channel header is passed"
-      }
-      return responseBuilder.formatResponse(event, 500, errResp)
+  try {
+    switch (event.headers.channel.toLowerCase()) {
+      case "connect":
+        return await connectProfilesHelper.connectProfileAction(event);
+      case "thirdpart":
+        return await connectProfilesHelper.connectProfileAction(event);
+      default:
+        return await connectProfilesHelper.connectProfileAction(event);
     }
-     
-  };
-  
+  } catch (error) {
+    console.error("index: error: ", error);
+    const errResp = {
+      errorMessage: "Check if channel header is passed",
+    };
+    return responseBuilder.formatResponse(event, 500, errResp);
+  }
+};
