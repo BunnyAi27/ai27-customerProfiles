082d87479e19b60b29a362f3ef4aaedc9a9246a3
BunnyAi27 <135638399+BunnyAi27@users.noreply.github.com>
2023-10-08 18:45:00 +0200

    Bug fix for import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const connectProfilesHelper = require("./helpers/connectProfilesHelper");
 const responseBuilder = require("./helpers/responseBuilder");
-const getTenantConfiguration = require("./helpers/getTenantConfiguration");
+const { getTenantConfiguration } = require("./helpers/getTenantConfig");
 
 exports.handler = async (event) => {
   // TODO implement
-  console.log("Event", JSON.stringify(event));
-  let tenantId = event.headers.tenantId.toLowerCase();
-  const tenantConfig = await getTenantConfiguration(tenantId);
+  console.info("Event", JSON.stringify(event));
+  const tenantId = event.headers.tenantId.toLowerCase();
+  const tenantConfig = await getTenantConfiguration.getTenantConfiguration(tenantId);
 
   try {
     switch (tenantConfig.Item.dataSource.toLowerCase()) {
@@ -20,7 +20,7 @@ exports.handler = async (event) => {
   } catch (error) {
     console.error("index: error: ", error);
     const errResp = {
-      errorMessage: "Check channel header",
+      errorMessage: "An Error occurred please contact your administrator",
     };
     return responseBuilder.formatResponse(event, 500, errResp);
   }
